b43d67e07867e1dd3309c752962c9f25f1321c2f
ykavyaaaa <kavya09072006@gmail.com>
2026-07-01 23:14:11 +0530

    Add files via upload

---
+++ pow(x,n) using recusion.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if(n==0):
+            return 1
+        if(n<0):
+           return self.myPow(1/x,-n)
+        if(n%2==0):
+           return  self.myPow(x*x,n//2)
+        else:
+           return x*(self.myPow(x*x,(n-1)//2))
